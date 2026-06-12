6f43bbaff627776806733ee6a8092c3de5fe2d21
davicomercial2024-lab <davicomercial2024-lab@users.noreply.github.com>
2026-06-12 09:16:44 -0300

    fix: untrack .vercel and fix gitignore

--- .vercel/output/static/assets/x-BA7pzu_g.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./portal-data-CaTDyXP-.js";var t=e(`pen`,[[`path`,{d:`M21.174 6.812a1 1 0 0 0-3.986-3.987L3.842 16.174a2 2 0 0 0-.5.83l-1.321 4.352a.5.5 0 0 0 .623.622l4.353-1.32a2 2 0 0 0 .83-.497z`,key:`1a8usu`}]]),n=e(`x`,[[`path`,{d:`M18 6 6 18`,key:`1bl5f8`}],[`path`,{d:`m6 6 12 12`,key:`d8bk6v`}]]);export{t as n,n as t};
